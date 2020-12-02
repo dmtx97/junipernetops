d04a35e61837b02094963d32e65421994ae94b99
Daniel Mendez <dmtx97@gmail.com>
2020-12-02 10:32:57 -0600

    updated setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
 setup(
     name = 'netopsauto',
     version = '0.1',
-    packages=find_packages('./netopsauto'),
+    packages=find_packages(),
     description='Useful tools to automate your network',
     long_description=long_description,
     long_description_content_type = 'text/markdown',
-    install_requires=['junos-eznc==2.5.4', 'netaddr==0.8.0', 'lxml==4.6.2', 'bcrypt==3.2.0', 'cryptography', 'textfsm', 'yamlordereddictloader==0.4.0', 'ncclient==0.6.9', 'scp==0.13.3', 'pyserial==3.5', 'transitions==0.8.5'],     
+    install_requires=['junos-eznc >=2.5.4', 'bcrypt >=3.2.0', 'cryptography >=3.2.1', 'textfsm >=0.3.2'],     
     url='https://github.com/dmtx97/netopsauto',
     author='Daniel Mendez',
     author_email='dmtx97@gmail.com'
